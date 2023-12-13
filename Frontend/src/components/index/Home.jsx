d51235a9dccf594e5da6ae17ce23e3f59bc8db68
unknown <balciunas.ed@gmail.com>
2023-12-13 15:00:29 +0200

    initial commit

---
+++ Frontend/src/components/index/Home.jsx
@@ -0,0 +1,17 @@
+import "./home.css";
+import FoodGroupButtons from "./FoodGroupButtons";
+import Menu from "./FoodGroup";
+
+function Home() {
+  return (
+    <>
+      <div className="max-md:hidden">
+        <FoodGroupButtons />
+      
+      </div>
+      <Menu />
+    </>
+  );
+}
+
+export default Home;
